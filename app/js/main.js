e2c743aeff857c8e985d5d5b75740fa0c5ab2bf1
Mustang <nubchenko@gmail.com>
2024-01-11 23:45:47 +0200

    added responsive design

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,46 @@
 $(function(){
 
+  function initSlick() {
+    $('.restaurants__list').slick({
+      arrows: false,
+      dots: true,
+      slidesToShow: 1
+    });
+  }
+
+  function destroySlick() {
+    if ($('.restaurants__list').hasClass('slick-initialized')) {
+      $('.restaurants__list').slick('unslick');
+    }
+  }
+
+  if (window.matchMedia("(max-width: 768px)").matches) {
+    initSlick();
+  }
+
+  $(window).resize(function () {
+    if (window.matchMedia("(max-width: 768px)").matches) {
+      initSlick();
+    } else {
+      destroySlick();
+    }
+  });
+
+  $('.burger-btn').on('click', function() {
+    $('.menu-burger').addClass('menu-burger--active');
+    $('.overlay').addClass('overlay--active');
+  });
+  
+  $('.menu-burger__btn').on('click', function() {
+    $('.menu-burger').removeClass('menu-burger--active');
+    $('.overlay').removeClass('overlay--active');
+  });
+  
+  $('.overlay').on('click', function() {
+    $('.menu-burger').removeClass('menu-burger--active'); 
+    $('.overlay').removeClass('overlay--active');
+  });
+
   $('.reviews__slider').slick({
     dots: true,
     arrows: true,
@@ -7,6 +48,14 @@ $(function(){
     fade: true,
     prevArrow: '<button type="button" class="slick-btn slick-prev"><svg class="arrow"><use xlink:href="images/icons/sprites/sprite.svg#prev-arrow"></use></svg></button>',
     nextArrow: '<button type="button" class="slick-btn slick-next"><svg class="arrow"><use xlink:href="images/icons/sprites/sprite.svg#prev-arrow"></use></svg></button>',
+    responsive: [
+      {
+        breakpoint: 561,
+        settings: {
+          dots: false
+        }
+      },
+    ]
   });
 
   $('.tab').on('click', function (e) {
